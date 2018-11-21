b419e2e4fe58e9046c2b9c30e8a424955f00ed0c
James Clark <jimmyjclark@gmail.com>
2018-11-20 17:25:58 -0800

    Init Commit
    Login and Registration views created

---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Jimmy Clark on 11/20/18.
+//  Copyright © 2018 James Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
