0329637585dd879c723b0fbc6f433aa94221c5fa
Danielle Nunez <dnunez@mail.sfsu.edu>
2018-12-01 17:23:24 -0800

    Podfiles installed for Firebase

---
+++ Login/Bridging-header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-header.h
+//  Messaging Demo
+//
+//  Created by Danielle Nunez on 11/27/18.
+//  Copyright © 2018 Danielle Nunez. All rights reserved.
+//
+
+#ifndef Bridging_header_h
+#define Bridging_header_h
+
+#import <JSQMessagesViewController/JSQMessages.h>
+
+#endif /* Bridging_header_h */
